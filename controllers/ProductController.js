3a20b95567ff30b5b0320ee49efe64c54296a082
Rogério de Sá <rogeriodesaf@gmail.com>
2022-10-08 17:57:50 -0300

    Inserindo dados no banco e visualizando no Compass

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,8 +1,25 @@
+const Product = require('../models/Products')
+const Products = require('../models/Products')
+
+
 module.exports =  class ProductController{
     static showProducts(req,res){
         res.render('products/all')
     }
-    static addProducts(req,res){
-        res.render('products/add')
+    static createProduct(req,res){
+        res.render('products/create')
+    }
+    static createProductPost(req,res){
+
+        const name = req.body.name
+        const price = req.body.price
+        const description = req.body.description
+
+        const product = new Product(name,price,description)
+        
+        product.save()
+
+        res.redirect('/products/')
+        
     }
 }
--- routers/productsRoutes.js
+++ routers/productsRoutes.js
@@ -3,6 +3,7 @@ const router  = express.Router()
 const ProductController = require('../controllers/ProductController')
 
 router.get('/',ProductController.showProducts)
-router.get('/adicionar',ProductController.addProducts)
+router.get('/create',ProductController.createProduct)
+router.post('/create',ProductController.createProductPost)
 
 module.exports = router
